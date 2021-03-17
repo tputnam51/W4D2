916efb1593b9c80031c1bbe3486ef66bbc3faa2f
Travis <tputnam8170@gmail.com>
2021-03-17 16:57:48 -0400

    Pawn class, forwward_dir and Forward_steps

--- Chess/pieces/pawn.rb
+++ Chess/pieces/pawn.rb
@@ -6,4 +6,34 @@ class Pawn < Piece
         '♙'.colorize(color)
     end
 
+    def moves
+
+    end
+
+    def at_start_row?
+
+    end
+
+    def forward_dir
+        if color == :white
+            1
+        else
+           -1
+        end
+    end
+
+    def forward_steps
+        y, x = pos
+        [[y + forward_dir, x ], [y + (2 * forward_dir), x]] = step
+
+        step.select do |pos|
+             self.board.valid_pos?(pos) && (board.empty?(pos) || self.color != board[pos].color)
+        end
+            
+    end
+
+    def side_attacks
+
+    end
+
 end
